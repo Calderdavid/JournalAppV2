9668706f40e48559cc041963f42db44989d84749
David Calderon <davidjcr200040@gmail.com>
2022-06-15 13:50:45 -0400

    config firebase

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,11 +2,31 @@ import { Link as RouterLink } from "react-router-dom"
 import { Google } from "@mui/icons-material"
 import { Button, Grid, TextField, Typography, Link } from "@mui/material"
 import { AuthLayout } from "../layout/AuthLayout"
+import { useForm } from "../../hooks/useForm"
+import { checkingAuthentication, startGoogleSignIn } from "../../store/auth"
+import { useDispatch } from "react-redux"
 
 export const LoginPage = () => {
+
+    const dispatch = useDispatch();
+    const {email, password, onInputChange} = useForm({
+        email: 'example@gmail.com',
+        password: '123456'
+    })
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        dispatch(checkingAuthentication())
+    }
+
+    const onGoogleSignIn = () => {
+        console.log('on google sign in')
+        dispatch(startGoogleSignIn())
+    }
+
     return(
         <AuthLayout title="Login">
-            <form>
+            <form onSubmit={onSubmit}>
                 <Grid container>
                     <Grid item xs={12} sx={{mt: 2}}>
                         <TextField 
@@ -14,6 +34,9 @@ export const LoginPage = () => {
                             type="email" 
                             placeholder="correo@gmail.com" 
                             fullWidth 
+                            name="email"
+                            value={email}
+                            onChange={onInputChange}
                         />
                     </Grid>
                     <Grid item xs={12} sx={{mt: 2}}>
@@ -22,17 +45,24 @@ export const LoginPage = () => {
                             type="password" 
                             placeholder="Contraseña" 
                             fullWidth 
+                            name="password"
+                            value={password}
+                            onChange={onInputChange}
                         />
                     </Grid>
 
                     <Grid container spacing={2} sx={{mb: 2, mt: 1}}>
                         <Grid item xs={12} sm={6}>
-                            <Button variant="contained" fullWidth>
+                            <Button type="submit" variant="contained" fullWidth>
                                 Login
                             </Button>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <Button variant="contained" fullWidth>
+                            <Button 
+                                variant="contained" 
+                                fullWidth
+                                onClick={onGoogleSignIn}
+                            >
                                 <Google />
                                 <Typography sx={{ml: 1}}>
                                     Google
---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
+import {getFirestore} from 'firebase/firestore/lite'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoWAxlIe-ZkqH-gefiFBWR4yFV-LjR3wA",
+  authDomain: "react-apps-a402f.firebaseapp.com",
+  projectId: "react-apps-a402f",
+  storageBucket: "react-apps-a402f.appspot.com",
+  messagingSenderId: "587812506162",
+  appId: "1:587812506162:web:68cf1c7674a277bfdc0a2b"
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+
+export const FirebaseAuth = getAuth(FirebaseApp);
+
+export const FirebaseDB = getFirestore(FiresbaseApp)
